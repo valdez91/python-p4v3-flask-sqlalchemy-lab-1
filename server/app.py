144e28dccb9538cf078f1326d5bc2fdba4149d32
Valdez91 <vickemtoo97@gmail.com>
2024-07-03 08:46:13 +0300

    code sol

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -14,14 +14,40 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify(
+            id=earthquake.id,
+            location=earthquake.location,
+            magnitude=earthquake.magnitude,
+            year=earthquake.year
+        ), 200
+    else:
+        return jsonify(message=f"Earthquake {id} not found."), 404
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes_list = [
+        {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        } for quake in earthquakes
+    ]
+    response_body = {
+        "count": len(quakes_list),
+        "quakes": quakes_list
+    }
+    return jsonify(response_body), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import Column, Integer, Float, String
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData()
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
 
-# Add models here
+    id = Column(Integer, primary_key=True)
+    magnitude = Column(Float, nullable=False)
+    location = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
